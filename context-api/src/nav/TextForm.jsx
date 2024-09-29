ed3b0075b12a9f5b33bb02093e3c0c4d4b2a3483
RiyaBanik003 <159024254+RiyaBanik003@users.noreply.github.com>
2024-09-29 13:00:00 +0530

    text-utils modifications

--- context-api/src/nav/TextForm.jsx
+++ context-api/src/nav/TextForm.jsx
@@ -2,6 +2,8 @@ import React, { useState } from 'react'
 function TextForm(props) {
     const [text, setText] = useState("")
     const [isUpperCase, setisUpperCase] = useState(false)
+    
+    
     const clrText=()=>{
         setText("")
     }
@@ -31,19 +33,19 @@ function TextForm(props) {
             <h1>{props.heading}</h1>
             <div className="mb-3">
                 <textarea className="form-control mb-3" id="exampleFormControlTextarea1" rows="3" value={text} onChange={handleChange} placeholder='Enter your text' >{toggleCase}</textarea>
-                <button className='btn btn-primary mx-2' onClick={clrText}>Clear text</button>
-                <button className="btn btn-primary" onClick={toggleCase}>
+                <button className='btn btn-primary mx-2 my-2' disabled={text.length===0} onClick={clrText}>Clear text</button>
+                <button className="btn btn-primary mx-2 my-2" disabled={text.length===0} onClick={toggleCase}>
                     {isUpperCase ? 'Convert to lowercase' : 'Convert to uppercase'}
                 </button>
-                <button className='btn btn-primary mx-2' onClick={handleCopy}>Copy text</button>
+                <button className='btn btn-primary mx-2 my-2' disabled={text.length===0} onClick={handleCopy}>Copy text</button>
 
 
             </div>
             <div>
 
                 <h1>{finalText.length} words and {text.length} characters</h1>
-                <h2>Preview: </h2>
-                <p>{text}</p>
+                <h2 >Preview: </h2>
+                <p>{text.length>0?text:"enter a text to preview"}</p>
             </div>
         </>
     )
